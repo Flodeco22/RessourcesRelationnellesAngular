d99e7ca7ac65f7b6652856a78d0a1e6befe744ba
Lefebvre <21902991@etu.unicaen.fr>
2022-04-04 00:49:23 +0200

    Ajout cellules actives dans le header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RedirectService } from '../services/redirect/redirect.service';
 import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {  
+export class HeaderComponent {
 
-  constructor(public redirectService: RedirectService, public authService: AuthService) { }
+  constructor(public redirectService: RedirectService, public authService: AuthService, public router: Router) { }
 
 }
